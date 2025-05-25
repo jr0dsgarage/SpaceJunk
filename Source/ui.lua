e8702c439cf9b174481cad6ae6c945b5f9ee1973
jr0dsgarage <jkozeal@gmail.com>
2025-05-24 19:08:42 -0600

    remove unnecessary comments

--- Source/ui.lua
+++ Source/ui.lua
@@ -7,17 +7,17 @@ local function drawScore(caught, missed, score)
     -- Draw one large black rectangle behind all label+value pairs
     gfx.setImageDrawMode(gfx.kDrawModeFillBlack)
     gfx.setColor(gfx.kColorBlack)
-    gfx.fillRect(0, 205, 400, 42) -- move up 12px (was 210)
+    gfx.fillRect(0, 205, 400, 42) 
 
     gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
     gfx.setColor(gfx.kColorWhite)
     gfx.setFont(fullCircleFont)
     -- Draw labels
-    gfx.drawTextAligned("CAUGHT", 0, 206, kTextAlignment.left)      -- was 218, now 218-12
+    gfx.drawTextAligned("CAUGHT", 0, 206, kTextAlignment.left)      
     gfx.drawTextAligned("SCORE", 200, 206, kTextAlignment.center)
     gfx.drawTextAligned("MISSED", 400, 206, kTextAlignment.right)
     -- Draw values below labels
-    gfx.drawTextAligned(tostring(caught), 0, 224, kTextAlignment.left)      -- was 236, now 236-12
+    gfx.drawTextAligned(tostring(caught), 0, 224, kTextAlignment.left)      
     gfx.drawTextAligned(tostring(score), 200, 224, kTextAlignment.center)
     gfx.drawTextAligned(tostring(missed), 400, 224, kTextAlignment.right)
 end
