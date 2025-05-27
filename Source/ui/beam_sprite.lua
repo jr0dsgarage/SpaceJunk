467d46c7a48e96cf799051c1ff0a92778a109948
jr0dsgarage <jkozeal@gmail.com>
2025-05-26 21:09:53 -0600

    use global screenwidth and screenheight

--- Source/ui/beam_sprite.lua
+++ Source/ui/beam_sprite.lua
@@ -12,7 +12,7 @@ function BeamSprite.new(scene)
     sprite:setCenter(0, 0)
     sprite:moveTo(0, 0)
     sprite:setZIndex(200) -- above flying objects
-    sprite:setSize(scene.screenWidth, scene.screenHeight)
+    sprite:setSize(_G.SCREEN_WIDTH, _G.SCREEN_HEIGHT)
     sprite.draw = function(_)
         local cx, cy = math.floor(scene.beamX + 0.5), math.floor(scene.beamY + 0.5)
         local innerRadius = math.floor(scene.beamRadius + 0.5)
