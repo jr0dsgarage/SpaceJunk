43997eeda93c28b1310f83f00ce71d0849fdf52c
jr0dsgarage <jkozeal@gmail.com>
2025-06-09 19:32:27 -0600

    make some tick marks larger

--- Source/ui/beam_zoom_sprite.lua
+++ Source/ui/beam_zoom_sprite.lua
@@ -9,7 +9,7 @@ BeamZoomSprite.__index = BeamZoomSprite
 function BeamZoomSprite.new(parentScene)
     local self = setmetatable({}, BeamZoomSprite)
     self.parentScene = parentScene
-    self.width = 5
+    self.width = 7
     self.largeTickPadding = 2 -- extra length for the larger tick                                                        -- padding for every 5th tick mark
     self.largeTickSpacing = 5 -- draw a larger tick mark every nth tick
     self.height = _G.SCREEN_HEIGHT - (_G.TIMERBAR_HEIGHT + _G.SCOREBOARD_HEIGHT) -- was -4, now -8 for 2px less on top and bottom
@@ -33,17 +33,23 @@ function BeamZoomSprite.new(parentScene)
         local maxBeam = self.parentScene.maxBeamRadius
         local beamPercent = (beamRadius - minBeam) / (maxBeam - minBeam)
         local nTicks = math.floor(minTicks + (1 - beamPercent) * (maxTicks - minTicks))
-        local normalTickLength = self.width
-        local largeTickLength = self.width + self.largeTickPadding
+        local normalTickLength = self.width - self.largeTickPadding
+        local largeTickLength = self.width
         for i = 0, nTicks do
             local norm = i / nTicks
             local density = 0.5 * (1 - math.cos(norm * math.pi))
             local y = math.floor(density * (self.height - 1))
+            local x0, x1
             if i % self.largeTickSpacing == 0 then
-                print("Large tick at " .. y)
-                gfx.drawLine(self.width - largeTickLength, y, self.width, y)
+                x0 = self.width
+                x1 = self.width - largeTickLength
+                print(string.format("Large tick at y=%d, x0=%d, x1=%d", y, x0, x1))
+                gfx.drawLine(x0, y, x1, y)
             else
-                gfx.drawLine(self.width - normalTickLength, y, self.width, y)
+                x0 = self.width
+                x1 = self.width - normalTickLength
+                print(string.format("Normal tick at y=%d, x0=%d, x1=%d", y, x0, x1))
+                gfx.drawLine(x0, y, x1, y)
             end
         end
     end
