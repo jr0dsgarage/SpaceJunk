057fd38394da4b5720ae42d32f5e6f4b9b176849
jr0dsgarage <jkozeal@gmail.com>
2025-06-14 15:42:57 -0600

    use transparent version of the space image

--- Source/graphics/load_space_image.lua
+++ Source/graphics/load_space_image.lua
@@ -1,8 +1,7 @@
 -- Helper to load a specific image from the space_images directory
 local gfx <const> = playdate.graphics
 local function loadSpaceImage()
-    local imgPath = "sprites/space_images/JWST Advanced Deep Extragalactic Survey_transparent.png"
-    print(string.format("Loading space image from: %s\n", imgPath))
+    local imgPath = "sprites/space_images/JWST Advanced Deep Extragalactic Survey.png"
     local ok, img = pcall(function() return gfx.image.new(imgPath) end)
     if ok and img then
         return img
