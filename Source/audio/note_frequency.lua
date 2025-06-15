e7092e0d2fd9b976a70df19003d9d18397e57f33
jr0dsgarage <jkozeal@gmail.com>
2025-06-14 18:58:12 -0600

    add a audio helper file for note frequencies

---
+++ Source/audio/note_frequency.lua
@@ -0,0 +1,35 @@
+---
+-- NoteFrequency module: defines musical note frequencies for use throughout the game.
+-- @module NoteFrequency
+
+local NoteFrequency = {}
+
+-- Octave 4 (Middle C = C4)
+NoteFrequency.C4  = 261.63
+NoteFrequency.CS4 = 277.18
+NoteFrequency.D4  = 293.66
+NoteFrequency.DS4 = 311.13
+NoteFrequency.E4  = 329.63
+NoteFrequency.F4  = 349.23
+NoteFrequency.FS4 = 369.99
+NoteFrequency.G4  = 392.00
+NoteFrequency.GS4 = 415.30
+NoteFrequency.A4  = 440.00
+NoteFrequency.AS4 = 466.16
+NoteFrequency.B4  = 493.88
+
+-- Octave 5
+NoteFrequency.C5  = 523.25
+NoteFrequency.CS5 = 554.37
+NoteFrequency.D5  = 587.33
+NoteFrequency.DS5 = 622.25
+NoteFrequency.E5  = 659.25
+NoteFrequency.F5  = 698.46
+NoteFrequency.FS5 = 739.99
+NoteFrequency.G5  = 783.99
+NoteFrequency.GS5 = 830.61
+NoteFrequency.A5  = 880.00
+NoteFrequency.AS5 = 932.33
+NoteFrequency.B5  = 987.77
+
+return NoteFrequency
--- Source/audio/sound_manager.lua
+++ Source/audio/sound_manager.lua
@@ -7,7 +7,7 @@
 --   local sm = SoundManager.new()
 
 local snd = playdate.sound -- Playdate sound module
-
+local nf = import("audio/note_frequency.lua") -- Musical note frequencies (alias: nf)
 local SoundManager = {} -- Table for SoundManager methods and metatable
 SoundManager.__index = SoundManager -- Metatable index for SoundManager
 
@@ -24,7 +24,7 @@ end
 -- @param precision Number between 0 and 1
 function SoundManager:playCapture(precision)
     -- precision: 0..1
-    local freq = 440 + 200 * precision
+    local freq = nf.A4 + 200 * precision
     self.captureSynth:playNote(freq, 0.2, 0.2)
 end
 
@@ -34,12 +34,12 @@ function SoundManager:playMiss()
     -- Use a short, low note with a bit of noise
     local synth = playdate.sound.synth.new(playdate.sound.kWaveNoise)
     synth:setADSR(0.01, 0.05, 0, 0.1)
-    synth:playNote(80, 0.15, 0.2) -- low frequency, short duration
+    synth:playNote(nf.E4 or 80, 0.15, 0.2) -- low frequency, short duration
 end
 
 --- Play a countdown beep with fade-out effect.
 function SoundManager:playCountdownBeep()
-    local BEEP_FREQ = 880 -- A5
+    local BEEP_FREQ = nf.A5 -- A5
     local BEEP_DURATION = 0.5
     local BEEP_VOLUME = 0.5
     local synth = playdate.sound.synth.new(playdate.sound.kWaveSquare)
@@ -55,7 +55,13 @@ function SoundManager:playCountdownBeep()
 end
 
 function SoundManager:playScoreTune(isHighScore)
-    local TUNE_BASE_FREQS = {523.25, 587.33, 659.25, 698.46, 783.99} -- C5, D5, E5, F5, G5
+    local TUNE_BASE_FREQS = {
+        nf.C5,
+        nf.D5,
+        nf.E5,
+        nf.F5,
+        nf.G5
+    }
     local TUNE_DURATION = 0.12
     local TUNE_VOLUME = 0.5
     local TUNE_OCTAVE_RATIO = 2 -- One octave up
