88d81b716a47b4547a3beeacb33b19fdc5d8b1a2
jr0dsgarage <jkozeal@gmail.com>
2025-06-17 13:09:28 -0600

    add more octaves

--- Source/audio/note_frequency.lua
+++ Source/audio/note_frequency.lua
@@ -4,6 +4,48 @@
 
 local NoteFrequency = {}
 
+-- Octave 1
+NoteFrequency.C1  = 32.70
+NoteFrequency.CS1 = 34.65
+NoteFrequency.D1  = 36.71
+NoteFrequency.DS1 = 38.89
+NoteFrequency.E1  = 41.20
+NoteFrequency.F1  = 43.65
+NoteFrequency.FS1 = 46.25
+NoteFrequency.G1  = 49.00
+NoteFrequency.GS1 = 51.91
+NoteFrequency.A1  = 55.00
+NoteFrequency.AS1 = 58.27
+NoteFrequency.B1  = 61.74
+
+-- Octave 2
+NoteFrequency.C2  = 65.41
+NoteFrequency.CS2 = 69.30
+NoteFrequency.D2  = 73.42
+NoteFrequency.DS2 = 77.78
+NoteFrequency.E2  = 82.41
+NoteFrequency.F2  = 87.31
+NoteFrequency.FS2 = 92.50
+NoteFrequency.G2  = 98.00
+NoteFrequency.GS2 = 103.83
+NoteFrequency.A2  = 110.00
+NoteFrequency.AS2 = 116.54
+NoteFrequency.B2  = 123.47
+
+-- Octave 3
+NoteFrequency.C3  = 130.81
+NoteFrequency.CS3 = 138.59
+NoteFrequency.D3  = 146.83
+NoteFrequency.DS3 = 155.56
+NoteFrequency.E3  = 164.81
+NoteFrequency.F3  = 174.61
+NoteFrequency.FS3 = 185.00
+NoteFrequency.G3  = 196.00
+NoteFrequency.GS3 = 207.65
+NoteFrequency.A3  = 220.00
+NoteFrequency.AS3 = 233.08
+NoteFrequency.B3  = 246.94
+
 -- Octave 4 (Middle C = C4)
 NoteFrequency.C4  = 261.63
 NoteFrequency.CS4 = 277.18
@@ -32,4 +74,19 @@ NoteFrequency.A5  = 880.00
 NoteFrequency.AS5 = 932.33
 NoteFrequency.B5  = 987.77
 
+-- Octave 6
+NoteFrequency.C6  = 1046.50
+NoteFrequency.CS6 = 1108.73
+NoteFrequency.D6  = 1174.66
+NoteFrequency.DS6 = 1244.51
+NoteFrequency.E6  = 1318.51
+NoteFrequency.F6  = 1396.91
+NoteFrequency.FS6 = 1479.98
+NoteFrequency.G6  = 1567.98
+NoteFrequency.GS6 = 1661.22
+NoteFrequency.A6  = 1760.00
+NoteFrequency.AS6 = 1864.66
+NoteFrequency.B6  = 1975.53
+
+_G.NoteFrequency = NoteFrequency
 return NoteFrequency
